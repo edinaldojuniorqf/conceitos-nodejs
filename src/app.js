811fd6881148bd5bcf4a95619313a72d855a62dc
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2020-06-18 22:26:48 -0300

    Desafio respondido

--- src/app.js
+++ src/app.js
@@ -1,33 +1,79 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
+const { uuid } = require('uuidv4')
 
-// const { uuid } = require("uuidv4");
+const app = express()
 
-const app = express();
+app.use(express.json())
+app.use(cors())
 
-app.use(express.json());
-app.use(cors());
+const repositories = []
 
-const repositories = [];
+app.get('/repositories', (request, response) => {
+  response.status(200).json(repositories)
+})
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  repositories.push(repo)
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  response.status(201).json(repo)
+})
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
 
-module.exports = app;
+  let repo = repositories.find(repo => repo.id === id)
+
+  if (repo === undefined) {
+    return response.status(400).send()
+  }
+
+  repo = {
+    ...repo,
+    title,
+    url,
+    techs
+  }
+
+  response.status(200).json(repo)
+})
+
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const index = repositories.findIndex(repo => repo.id === id)
+
+  if (index === -1) {
+    return response.status(400).send()
+  }
+
+  repositories.splice(index, 1)
+
+  response.status(204).send()
+})
+
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params
+
+  let repo = repositories.find(repo => repo.id === id)
+
+  if (repo === undefined) {
+    return response.status(400).send()
+  }
+
+  repo.likes += 1
+
+  response.status(201).json(repo)
+})
+
+module.exports = app
